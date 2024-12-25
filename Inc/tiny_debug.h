bd9a2730c48892f0f239f9015bbe0d28f9f580fb
Amir.Momen <amir.momen69@gmail.com>
2024-12-25 13:50:57 +0330

    It was Tested and verified by stm32 IDE

---
+++ Inc/tiny_debug.h
@@ -0,0 +1,63 @@
+/*
+ * tiny_debug.h
+ *
+ *  Created on: Dec 25, 2024
+ *      Author: amiro
+ */
+
+#ifndef TINY_DEBUG_TINY_DEBUG_H_
+#define TINY_DEBUG_TINY_DEBUG_H_
+
+//#define ENABLE_GLOBAL_DEBUG
+
+#include <stdio.h>
+
+#define NEWLINE       "\r\n"
+
+#if defined(ENABLE_GLOBAL_DEBUG) || defined(DEBUG_THIS_FILE)
+
+	#define FILENAME_PATH_OMMITED 		(strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+	#define PLACE(x)                    do{x} while(0)
+	#define PRINT(...)                  printf(__VA_ARGS__)
+
+	#define DEBUG_FUNC(string, ...)          PRINT(string, ##__VA_ARGS__)
+	#define DEBUG_LN(...)               	{   DEBUG_FUNC(__VA_ARGS__);\
+												DEBUG_FUNC(NEWLINE); }
+
+	#define DEBUG_PRINT_HEADER(HEADER)               DEBUG_FUNC(" + [" #HEADER "] \t: ")
+	#define DEBUG_PRINT_MSG(HEADER, MSG, ...)        DEBUG_FUNC(" + [" #HEADER "] \t: " MSG NEWLINE, ##__VA_ARGS__)
+
+
+	#define DEBUG_OK(...)       DEBUG_PRINT_MSG(OK, __VA_ARGS__)
+	#define DEBUG_ALERT(...)  	DEBUG_PRINT_MSG(ALERT, __VA_ARGS__)
+	#define DEBUG_WARNING(...)  DEBUG_PRINT_MSG(WARN, __VA_ARGS__)
+	#define DEBUG_TRACE()       DEBUG_FUNC(" + [TRACE] \t: File : %s :: Function : %s() :: Line : %d" NEWLINE, FILENAME_PATH_OMMITED, __func__, __LINE__)
+	#define DEBUG_ERROR(...)    	PLACE(\
+										DEBUG_PRINT_HEADER(ERROR);     \
+										DEBUG_LN(__VA_ARGS__);   \
+										DEBUG_TRACE();  \
+										)
+	#define DEBUG_EXCEPTION(...)    PLACE(\
+										DEBUG_PRINT_HEADER(EXCEPTION);     \
+										DEBUG_LN(__VA_ARGS__);   \
+										DEBUG_TRACE();  \
+										)
+
+
+#else
+	#define PLACE(x)
+	#define PRINT(...)
+	#define DEBUG_FUNC(string, ...)
+	#define DEBUG_LN(...)
+	#define DEBUG_PRINT_HEADER(HEADER)
+	#define DEBUG_PRINT_MSG(HEADER, MSG, ...)
+	#define DEBUG_OK(...)
+	#define DEBUG_ERROR(...)
+	#define DEBUG_ALERT(...)
+	#define DEBUG_WARNING(...)
+	#define DEBUG_TRACE()
+	#define DEBUG_EXCEPTION(...)
+#endif /* defined(ENABLE_GLOBAL_DEBUG) || defined(DEBUG_THIS_FILE) */
+
+#endif /* TINY_DEBUG_TINY_DEBUG_H_ */
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Tiny_Debug
-A debug library written in C/C++ 
+A debug library written in **C/C++**. 
+It is designed for **embedded systems**
+  - Low memory usage
+  - Very easy to use
+
+  ## How to use
+
+- **Print OK Message :**
+```C
+	DEBUG_OK("OK Message");
+```
+
+- **Print ERROR Message :**
+```C
+	DEBUG_ERROR("ERROR Message");
+```
+
+- **Print ALERT Message :**
+```C
+	DEBUG_ALERT("ALERT Message From %s", "Fire Station");
+```
+
+- **Print WARNING Message :**
+```C
+	DEBUG_WARNING("WARNING Message");
+```
+
+**Print Current Location in Code :**
+```C
+	DEBUG_TRACE();
+```	
+
